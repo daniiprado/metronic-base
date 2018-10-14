e1746df5a02ac6a1b2d5ced802d1aa071692260b
Daniel Prado <danii.pradooutlook.com.ar>
2018-10-14 00:39:23 -0500

    Mod

--- resources/lang/es/validation.php
+++ resources/lang/es/validation.php
@@ -254,8 +254,8 @@ return [
         'number'                => 'número',
         'received'              => 'recibido',
         'delivered'             => 'entregado',
-        'done with novelty'     => 'finalizado con novedad',
-        'in transit'            => 'en tránsito',
-        'in transit with novelty'   => 'en tránsito con novedad',
+        //'done with novelty'     => 'finalizado con novedad',
+        //'in transit'            => 'en tránsito',
+        //'in transit with novelty'   => 'en tránsito con novedad',
     ],
 ];
